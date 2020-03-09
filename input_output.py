1ab9bb9a2c45ad96b0a8333bdac5fe733c3a35e5
Небожитель <gjvbljhrf08@mail.ru>
2020-03-09 15:52:57 +0300

    tt3

---
+++ input_output.py
@@ -0,0 +1,10 @@
+
+
+
+#Ввод
+
+name = input('Введи фразу')
+print(name)
+age = input('Введи возраст ')
+print(age, type(age))
+print(int(age), type(int(age)))
---
+++ math_operation.py
@@ -0,0 +1,35 @@
+
+a = input('First number ')
+b = input('Second number ')
+
+a = int(a)
+b = int(b)
+
+print(a + b)
+print(a - b)
+print(a * b)
+result = a / b
+print(type(result))
+print(result)
+
+print(a ** b)
+
+#Логические операции
+a = True
+b = False
+
+# Отрицание
+print(not a)
+
+# Логическое И
+print(a and b)
+# Логическое ИЛИ
+print(a or b)
+
+a = 10
+print(a > 100)
+print(a < 100)
+print(a <= 100)
+print(a >= 100)
+print(a == 100)
+print(a != 100)
